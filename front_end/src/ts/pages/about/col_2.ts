d753cecabde6c30537453f0a246aaf084bfda58e
wild_duck <wezonaser50@gmail.com>
2024-04-07 21:22:38 +0200

    style: about page responsive

--- front_end/src/ts/pages/about/col_2.ts
+++ front_end/src/ts/pages/about/col_2.ts
@@ -1,115 +1,121 @@
-import gsap from 'gsap';
-import SplitType from 'split-type';
+import gsap from 'gsap'
+import SplitType from 'split-type'
 
 export const col_2 = () => {
-    const col_2 = document.querySelector('.col__1__btn__wrapper__btn') as HTMLDivElement;
+    const col_2 = document.querySelector('.col__1__btn__wrapper__btn') as HTMLDivElement
 
-    if (!col_2) return;
+    if (!col_2) return
 
     const TWords = new SplitType('.col__2__title', {
         types: 'lines,words'
-    }).words;
+    }).words
 
-    gsap.set('.col__2__span', {
-        yPercent: 170
-    });
+    const mm = gsap.matchMedia()
 
-    gsap.set('.col__2__pin', {
-        overflow: 'hidden'
-    });
+    mm.add('(min-width: 768px)', () => {
+        gsap.context(() => {
+            gsap.set('.col__2__span', {
+                yPercent: 170
+            })
 
-    gsap.set('.col__2__title', {
-        position: 'relative',
-        background: 'white',
-        overflow: 'hidden',
-        zIndex: 3
-    });
+            gsap.set('.col__2__pin', {
+                overflow: 'hidden'
+            })
 
-    gsap.set('.col__2__svg', {
-        yPercent: 150
-    });
+            gsap.set('.col__2__title', {
+                position: 'relative',
+                background: 'white',
+                overflow: 'hidden',
+                zIndex: 3
+            })
 
-    gsap.set(TWords, {
-        yPercent: 100
-    });
+            gsap.set('.col__2__svg', {
+                yPercent: 150
+            })
 
-    gsap.set('.our__team img', {
-        scale: 0.5,
-        opacity: 0
-    });
+            gsap.set(TWords, {
+                yPercent: 100
+            })
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.col__2__title',
-            start: 'top bottom',
-            end: '650px bottom',
-            scrub: 0.7
-        }
-    });
+            gsap.set('.our__team img', {
+                scale: 0.5,
+                opacity: 0
+            })
 
-    tl.to('.col__2__span', {
-        yPercent: 0,
-        ease: 'Power1.easeInOut',
-        delay: 0.2
-    });
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.col__2__title',
+                    start: 'top bottom',
+                    end: '650px bottom',
+                    scrub: 0.7
+                }
+            })
 
-    tl.to(
-        TWords,
-        {
-            yPercent: 0,
-            stagger: { amount: 0.6 },
-            ease: 'Power1.easeInOut',
-            delay: 0.2
-        },
-        '<'
-    );
+            tl.to('.col__2__span', {
+                yPercent: 0,
+                ease: 'Power1.easeInOut',
+                delay: 0.2
+            })
 
-    tl.to(
-        '.col__2__svg',
-        {
-            yPercent: 0,
-            ease: 'Power1.easeInOut',
-            delay: 0.2
-        },
-        '<'
-    );
+            tl.to(
+                TWords,
+                {
+                    yPercent: 0,
+                    stagger: { amount: 0.6 },
+                    ease: 'Power1.easeInOut',
+                    delay: 0.2
+                },
+                '<'
+            )
 
-    tl.set('.col__2.about__col > div', {
-        overflow: 'initial'
-    });
+            tl.to(
+                '.col__2__svg',
+                {
+                    yPercent: 0,
+                    ease: 'Power1.easeInOut',
+                    delay: 0.2
+                },
+                '<'
+            )
 
-    // NOTE: imgs
+            tl.set('.col__2.about__col > div', {
+                overflow: 'initial'
+            })
 
-    const tl2 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.col__2',
-            start: 'clamp(-10% 98%)',
-            end: 'clamp(110% bottom)',
-            scrub: 0.5
-            // markers: true
-        }
-    });
+            // NOTE: imgs
 
-    tl2.to(
-        '.our__team img',
-        {
-            scale: 1,
-            opacity: 1,
-            stagger: { amount: 0.8 },
-            ease: 'Power1.easeInOut'
-        },
-        '<'
-    );
+            const tl2 = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.col__2',
+                    start: 'clamp(-10% 98%)',
+                    end: 'clamp(110% bottom)',
+                    scrub: 0.5
+                    // markers: true
+                }
+            })
 
-    // NOTE: pin
-    const tl3 = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.col__2',
-            start: 'top 40%',
-            end: 'bottom bottom',
-            pin: '.col__2__pin',
-            pinSpacing: true,
-            scrub: 0.7
-        }
-    });
-};
+            tl2.to(
+                '.our__team img',
+                {
+                    scale: 1,
+                    opacity: 1,
+                    stagger: { amount: 0.8 },
+                    ease: 'Power1.easeInOut'
+                },
+                '<'
+            )
+
+            // NOTE: pin
+            const tl3 = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.col__2',
+                    start: 'top 40%',
+                    end: 'bottom bottom',
+                    pin: '.col__2__pin',
+                    pinSpacing: true,
+                    scrub: 0.7
+                }
+            })
+        })
+    })
+}
