4bb91ce5bde245b19be15e3508359d2791274303
wild_duck <wezonaser50@gmail.com>
2024-04-07 21:21:29 +0200

    style: credits page responsive

--- front_end/src/ts/animations/other_pages.ts
+++ front_end/src/ts/animations/other_pages.ts
@@ -1,27 +1,32 @@
-import gsap from 'gsap';
+import gsap from 'gsap'
 
 export const other_pages = () => {
-    const onceInView = document.querySelector('.once-in-view') as HTMLDivElement;
-    if (!onceInView) return;
+    const onceInView = document.querySelector('.once-in-view') as HTMLDivElement
+    if (!onceInView) return
 
-    gsap.set('.once-in-view img', {
-        // y: 100,
-        scale: 1.4
-    });
+    const mm = gsap.matchMedia()
+    mm.add('(min-width: 768px)', () => {
+        gsap.context(() => {
+            gsap.set('.once-in-view img', {
+                // y: 100,
+                scale: 1.4
+            })
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.page__hero__section',
-            start: '-10% top',
-            end: '230% bottom',
-            scrub: true
-            // markers: true
-        }
-    });
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: '.page__hero__section',
+                    start: '-10% top',
+                    end: '230% bottom',
+                    scrub: true
+                    // markers: true
+                }
+            })
 
-    tl.to('.once-in-view img', {
-        y: 0,
-        scale: 1,
-        ease: 'Power1.easeInOut'
-    });
-};
+            tl.to('.once-in-view img', {
+                y: 0,
+                scale: 1,
+                ease: 'Power1.easeInOut'
+            })
+        })
+    })
+}
