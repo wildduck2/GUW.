a0c9abd9f9682e440873d7f9c0ce44cc841a2ca6
wild_duck <wezonaser50@gmail.com>
2024-04-07 21:23:49 +0200

    style: works page respoinsive

--- front_end/src/ts/layouts/text__section.ts
+++ front_end/src/ts/layouts/text__section.ts
@@ -8,60 +8,65 @@ export const text__section = () => {
     new SplitType('.pin__section h2', {
         types: 'lines,words'
     })
-
     if (!document.querySelector('.pin__section')) return
-    gsap.set('.pin__section h2 .word', {
-        yPercent: 100
-    })
 
-    gsap.set('.pin__section h2 .line', {
-        overflow: 'hidden'
-    })
+    const mm = gsap.matchMedia()
+    // mm.add('(min-width: 768px)', () => {
+    gsap.context(() => {
+        gsap.set('.pin__section h2 .word', {
+            yPercent: 100
+        })
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: '.pin__section',
-            start: 'top top',
-            end: 'clamp(+=3400 bottom)',
-            pin: '.pin__section',
-            scrub: true
-        }
-    })
+        gsap.set('.pin__section h2 .line', {
+            overflow: 'hidden'
+        })
 
-    tl.to('.col__2__title-1 .word', {
-        yPercent: 0,
-        stagger: { amount: 0.8 },
-        ease: 'Power1.inOut'
-    })
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.pin__section',
+                start: 'top top',
+                end: 'clamp(+=3400, bottom)',
+                pin: '.pin__section',
+                scrub: true
+            }
+        })
 
-    tl.to('.col__2__title-1 .word', {
-        yPercent: -100,
-        stagger: { amount: 0.8 },
-        ease: 'Power1.inOut'
-    })
+        tl.to('.col__2__title-1 .word', {
+            yPercent: 0,
+            stagger: { amount: 0.8 },
+            ease: 'Power1.inOut'
+        })
 
-    tl.to('.col__2__title-2 .word', {
-        yPercent: 0,
-        stagger: { amount: 0.8 },
-        ease: 'Power1.inOut'
-    })
+        tl.to('.col__2__title-1 .word', {
+            yPercent: -100,
+            stagger: { amount: 0.8 },
+            ease: 'Power1.inOut'
+        })
 
-    tl.to('.col__2__title-2 .word', {
-        yPercent: -100,
-        stagger: { amount: 0.8 },
-        ease: 'Power1.inOut'
-    })
+        tl.to('.col__2__title-2 .word', {
+            yPercent: 0,
+            stagger: { amount: 0.8 },
+            ease: 'Power1.inOut'
+        })
 
-    if (!document.querySelector('.col__2__title-3')) return
-    tl.to('.col__2__title-3 .word', {
-        yPercent: 0,
-        stagger: { amount: 0.8 },
-        ease: 'Power1.inOut'
-    })
+        tl.to('.col__2__title-2 .word', {
+            yPercent: -100,
+            stagger: { amount: 0.8 },
+            ease: 'Power1.inOut'
+        })
 
-    tl.to('.col__2__title-3 .word', {
-        yPercent: -100,
-        stagger: { amount: 0.8 },
-        ease: 'Power1.inOut'
+        if (!document.querySelector('.col__2__title-3')) return
+        tl.to('.col__2__title-3 .word', {
+            yPercent: 0,
+            stagger: { amount: 0.8 },
+            ease: 'Power1.inOut'
+        })
+
+        tl.to('.col__2__title-3 .word', {
+            yPercent: -100,
+            stagger: { amount: 0.8 },
+            ease: 'Power1.inOut'
+        })
     })
+    // })
 }
