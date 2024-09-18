403d9eaf5dca0951050e893defde56bce39e5d9b
wild_duck <wezonaser50@gmail.com>
2024-09-18 11:03:25 +0300

    feat: adding missed code idk what id does

--- src/ts/pages/about/col_2.ts
+++ src/ts/pages/about/col_2.ts
@@ -2,120 +2,121 @@ import gsap from 'gsap'
 import SplitType from 'split-type'
 
 export const col_2 = () => {
-    const col_2 = document.querySelector('.col__1__btn__wrapper__btn') as HTMLDivElement
+  const col_2 = document.querySelector('.col__1__btn__wrapper__btn') as HTMLDivElement
 
-    if (!col_2) return
+  if (!col_2) return
 
-    const TWords = new SplitType('.col__2__title', {
-        types: 'lines,words'
-    }).words
+  const TWords = new SplitType('.col__2__title', {
+    types: 'lines,words'
+  }).words
 
-    const mm = gsap.matchMedia()
+  const mm = gsap.matchMedia()
 
-    mm.add('(min-width: 768px)', () => {
-        gsap.context(() => {
-            gsap.set('.col__2__span', {
-                yPercent: 170
-            })
+  mm.add('(min-width: 768px)', () => {
+    gsap.context(() => {
+      gsap.set('.col__2__span', {
+        yPercent: 170
+      })
 
-            gsap.set('.col__2__pin', {
-                overflow: 'hidden'
-            })
+      gsap.set('.col__2__pin', {
+        overflow: 'hidden'
+      })
 
-            gsap.set('.col__2__title', {
-                position: 'relative',
-                background: 'white',
-                overflow: 'hidden',
-                zIndex: 3
-            })
+      gsap.set('.col__2__title', {
+        position: 'relative',
+        background: 'white',
+        overflow: 'hidden',
+        zIndex: 3
+      })
 
-            gsap.set('.col__2__svg', {
-                yPercent: 150
-            })
+      gsap.set('.col__2__svg', {
+        yPercent: 150
+      })
 
-            gsap.set(TWords, {
-                yPercent: 100
-            })
+      gsap.set(TWords, {
+        yPercent: 100
+      })
 
-            gsap.set('.our__team img', {
-                scale: 0.5,
-                opacity: 0
-            })
+      gsap.set('.our__team img', {
+        scale: 0.5,
+        opacity: 0
+      })
 
-            const tl = gsap.timeline({
-                scrollTrigger: {
-                    trigger: '.col__2__title',
-                    start: 'top bottom',
-                    end: '650px bottom',
-                    scrub: 0.7
-                }
-            })
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.col__2__title',
+          start: 'top bottom',
+          end: '650px bottom',
+          scrub: 0.7
+        }
+      })
 
-            tl.to('.col__2__span', {
-                yPercent: 0,
-                ease: 'Power1.easeInOut',
-                delay: 0.2
-            })
+      tl.to('.col__2__span', {
+        yPercent: 0,
+        ease: 'Power1.easeInOut',
+        delay: 0.2
+      })
 
-            tl.to(
-                TWords,
-                {
-                    yPercent: 0,
-                    stagger: { amount: 0.6 },
-                    ease: 'Power1.easeInOut',
-                    delay: 0.2
-                },
-                '<'
-            )
+      tl.to(
+        TWords,
+        {
+          yPercent: 0,
+          stagger: { amount: 0.6 },
+          ease: 'Power1.easeInOut',
+          delay: 0.2
+        },
+        '<'
+      )
 
-            tl.to(
-                '.col__2__svg',
-                {
-                    yPercent: 0,
-                    ease: 'Power1.easeInOut',
-                    delay: 0.2
-                },
-                '<'
-            )
+      tl.to(
+        '.col__2__svg',
+        {
+          yPercent: 0,
+          ease: 'Power1.easeInOut',
+          delay: 0.2
+        },
+        '<'
+      )
 
-            tl.set('.col__2.about__col > div', {
-                overflow: 'initial'
-            })
+      tl.set('.col__2.about__col > div', {
+        overflow: 'initial'
+      })
 
-            // NOTE: imgs
+      // NOTE: imgs
 
-            const tl2 = gsap.timeline({
-                scrollTrigger: {
-                    trigger: '.col__2',
-                    start: 'clamp(-10% 98%)',
-                    end: 'clamp(110% bottom)',
-                    scrub: 0.5
-                    // markers: true
-                }
-            })
+      const tl2 = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.col__2',
+          start: 'clamp(-10% 98%)',
+          end: 'clamp(110% bottom)',
+          scrub: 0.5
+          // markers: true
+        }
+      })
 
-            tl2.to(
-                '.our__team img',
-                {
-                    scale: 1,
-                    opacity: 1,
-                    stagger: { amount: 0.8 },
-                    ease: 'Power1.easeInOut'
-                },
-                '<'
-            )
+      tl2.to(
+        '.our__team img',
+        {
+          scale: 1,
+          opacity: 1,
+          stagger: { amount: 0.8 },
+          ease: 'Power1.easeInOut'
+        },
+        '<'
+      )
 
-            // NOTE: pin
-            gsap.timeline({
-                scrollTrigger: {
-                    trigger: '.col__2',
-                    start: 'top 40%',
-                    end: 'bottom bottom',
-                    pin: '.col__2__pin',
-                    // pinSpacing: true,
-                    scrub: 0.1
-                }
-            })
-        })
+      // NOTE: pin
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: '.col__2',
+          start: 'top 40%',
+          end: 'bottom bottom',
+          pin: '.col__2__pin',
+          // pinSpacing: true,
+          scrub: 0.1,
+          markers: true
+        }
+      })
     })
+  })
 }
--- src/ts/pages/home/hero_section.ts
+++ src/ts/pages/home/hero_section.ts
@@ -2,88 +2,89 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 
 export const hero_animation = () => {
-    gsap.registerPlugin(ScrollTrigger)
+  gsap.registerPlugin(ScrollTrigger)
 
-    const el = document.querySelector('.hero__section') as HTMLDivElement
+  const el = document.querySelector('.hero__section') as HTMLDivElement
 
-    if (!el) return
-    const mm = gsap.matchMedia()
+  if (!el) return
+  const mm = gsap.matchMedia()
 
-    mm.add('(min-width: 768px)', () => {
-        gsap.context(() => {
-            const tl = gsap.timeline({
-                scrollTrigger: {
-                    trigger: '.hero__section',
-                    start: '-10% top',
-                    end: '180% bottom',
-                    scrub: 1,
-                    toggleActions: 'play none none none'
-                },
-                defaults: {
-                    ease: 'power1.out'
-                }
-            })
+  mm.add('(min-width: 768px)', () => {
+    gsap.context(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: '.hero__section',
+          start: '-10% top',
+          end: '180% bottom',
+          scrub: 1,
+          toggleActions: 'play none none none',
+          markers: true
+        },
+        defaults: {
+          ease: 'power1.out'
+        }
+      })
 
-            tl.set('.hero__section__right__bottom', {
-                overflow: 'initial'
-            })
+      tl.set('.hero__section__right__bottom', {
+        overflow: 'initial'
+      })
 
-            tl.set('.hero__section__right__bottom > div:first-child', {
-                overflow: 'initial'
-            })
+      tl.set('.hero__section__right__bottom > div:first-child', {
+        overflow: 'initial'
+      })
 
-            tl.to('.hero__section__right__top h1', {
-                yPercent: -20
-            })
+      tl.to('.hero__section__right__top h1', {
+        yPercent: -20
+      })
 
-            tl.to(
-                '.hero__section__right__top p',
-                {
-                    yPercent: -40
-                },
-                '<'
-            )
+      tl.to(
+        '.hero__section__right__top p',
+        {
+          yPercent: -40
+        },
+        '<'
+      )
 
-            tl.to(
-                '.hero__section__right__top a',
-                {
-                    yPercent: '-=30'
-                },
-                '<'
-            )
+      tl.to(
+        '.hero__section__right__top a',
+        {
+          yPercent: '-=30'
+        },
+        '<'
+      )
 
-            tl.to(
-                '.once-in-t',
-                {
-                    yPercent: '-=30',
-                    stagger: 0.2
-                },
-                '<'
-            )
+      tl.to(
+        '.once-in-t',
+        {
+          yPercent: '-=30',
+          stagger: 0.2
+        },
+        '<'
+      )
 
-            tl.to(
-                '.hero__section__left__img',
-                {
-                    yPercent: '-=5'
-                },
-                '<'
-            )
+      tl.to(
+        '.hero__section__left__img',
+        {
+          yPercent: '-=5'
+        },
+        '<'
+      )
 
-            tl.to(
-                '.hero__section__left__svg',
-                {
-                    yPercent: '-=10'
-                },
-                '<'
-            )
+      tl.to(
+        '.hero__section__left__svg',
+        {
+          yPercent: '-=10'
+        },
+        '<'
+      )
 
-            tl.to(
-                '.hero__section__left__img img',
-                {
-                    yPercent: -15
-                },
-                '<'
-            )
-        })
+      tl.to(
+        '.hero__section__left__img img',
+        {
+          yPercent: -15
+        },
+        '<'
+      )
     })
+  })
 }
