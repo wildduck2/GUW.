0fcbc98f35278b7f430f597501123b465b102c94
wildduck2 <wezonaser50@gmail.com>
2025-07-16 08:54:46 +0300

    build(all): ignore

--- src/ts/animations/initBarbaAnimation.ts
+++ src/ts/animations/initBarbaAnimation.ts
@@ -1,97 +1,98 @@
-import barba from '@barba/core'
+// @ts-ignore
+import barba from "@barba/core";
 import {
-    delay,
-    initLoaderHome,
-    initNextWord,
-    initPageEnter,
-    initPageLeave,
-    initPageOnce
-} from './initLoading'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
-import Lenis from '@studio-freight/lenis'
-import { initALLScripts } from '../main'
+	delay,
+	initLoaderHome,
+	initNextWord,
+	initPageEnter,
+	initPageLeave,
+	initPageOnce,
+} from "./initLoading";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import Lenis from "@studio-freight/lenis";
+import { initALLScripts } from "../main";
 
 export const initBarbaAnimation = () => {
-    //NOTE: init lenis
-    gsap.registerPlugin(ScrollTrigger)
+	//NOTE: init lenis
+	gsap.registerPlugin(ScrollTrigger);
 
-    const lenis = new Lenis({
-        lerp: 0.1,
-        syncTouchLerp: 0.1,
-        smoothWheel: true
-    })
-    //TODO: make sure to re init the scroll thing when window resize
+	const lenis = new Lenis({
+		lerp: 0.1,
+		syncTouchLerp: 0.1,
+		smoothWheel: true,
+	});
+	//TODO: make sure to re init the scroll thing when window resize
 
-    lenis.on('scroll', ScrollTrigger.update)
+	lenis.on("scroll", ScrollTrigger.update);
 
-    gsap.ticker.add((time) => {
-        lenis.raf(time * 1000)
-    })
+	gsap.ticker.add((time) => {
+		lenis.raf(time * 1000);
+	});
 
-    gsap.ticker.lagSmoothing(0)
+	gsap.ticker.lagSmoothing(0);
 
-    // NOTE: do something before the transition starts
-    barba.hooks.before(() => {
-        document.querySelector('html')!.classList.add('is-transitioning')
-    })
+	// NOTE: do something before the transition starts
+	barba.hooks.before(() => {
+		document.querySelector("html")!.classList.add("is-transitioning");
+	});
 
-    // NOTE: do something after the transition finishes
-    barba.hooks.after(() => {
-        document.querySelector('html')!.classList.remove('is-transitioning')
-    })
+	// NOTE: do something after the transition finishes
+	barba.hooks.after(() => {
+		document.querySelector("html")!.classList.remove("is-transitioning");
+	});
 
-    //NOTE: scroll to the top of the page
-    barba.hooks.afterEnter(() => {
-        window.scrollTo(0, 0)
-        //TODO: cookie functionality
-    })
+	//NOTE: scroll to the top of the page
+	barba.hooks.afterEnter(() => {
+		window.scrollTo(0, 0);
+		//TODO: cookie functionality
+	});
 
-    // TODO: idk what to call it but it's a clearing thing
-    // if (window.innerWidth > 540) {
-    //     barba.hooks.leave(() => {
-    //         document.querySelectorAll('.btn-hamburger, .btn-menu').forEach((el) => {
-    //             el.classList.remove('active');
-    //         });
-    //         document.querySelector('main')!.classList.remove('nav-active');
-    //     });
-    // }
+	// TODO: idk what to call it but it's a clearing thing
+	// if (window.innerWidth > 540) {
+	//     barba.hooks.leave(() => {
+	//         document.querySelectorAll('.btn-hamburger, .btn-menu').forEach((el) => {
+	//             el.classList.remove('active');
+	//         });
+	//         document.querySelector('main')!.classList.remove('nav-active');
+	//     });
+	// }
 
-    barba.init({
-        debug: false,
-        timeout: 7000,
-        transitions: [
-            {
-                name: 'works',
-                async once() {
-                    initPageOnce()
-                },
-                async leave({ current }) {
-                    // NOTE: animate loading screen in
-                    initPageLeave()
-                    await delay(695)
-                    current.container.remove()
-                },
-                async enter(data) {
-                    // NOTE: animate loading screen away
-                    initNextWord(data)
-                    initPageEnter()
-                },
-                async beforeEnter() {
-                    ScrollTrigger.getAll().forEach((t) => t.kill())
-                    initALLScripts()
-                }
-            },
-            {
-                name: 'initHomeLoader',
-                from: {},
-                to: {
-                    namespace: ['home']
-                },
-                once() {
-                    initLoaderHome()
-                }
-            }
-        ]
-    })
-}
+	barba.init({
+		debug: false,
+		timeout: 7000,
+		transitions: [
+			{
+				name: "works",
+				async once() {
+					initPageOnce();
+				},
+				async leave({ current }: any) {
+					// NOTE: animate loading screen in
+					initPageLeave();
+					await delay(695);
+					current.container.remove();
+				},
+				async enter(data: any) {
+					// NOTE: animate loading screen away
+					initNextWord(data);
+					initPageEnter();
+				},
+				async beforeEnter() {
+					ScrollTrigger.getAll().forEach((t) => t.kill());
+					initALLScripts();
+				},
+			},
+			{
+				name: "initHomeLoader",
+				from: {},
+				to: {
+					namespace: ["home"],
+				},
+				once() {
+					initLoaderHome();
+				},
+			},
+		],
+	});
+};
--- src/ts/animations/initLoading.ts
+++ src/ts/animations/initLoading.ts
@@ -1,233 +1,234 @@
-import { ITransitionData } from '@barba/core'
-import { gsap } from 'gsap/gsap-core'
-import SplitType from 'split-type'
-import { initHomePage, initHomePageAnimations } from './home_animations'
+// @ts-ignore
+import { ITransitionData } from "@barba/core";
+import { gsap } from "gsap/gsap-core";
+import SplitType from "split-type";
+import { initHomePage, initHomePageAnimations } from "./home_animations";
 
 export const initPageOnce = () => {
-    const tl = gsap.timeline({})
+	const tl = gsap.timeline({});
 
-    new SplitType('.arabic', { types: 'lines,words' })
+	new SplitType(".arabic", { types: "lines,words" });
 
-    tl.to('.loader__h2 .word', {
-        y: 150
-    })
+	tl.to(".loader__h2 .word", {
+		y: 150,
+	});
 
-    tl.set('.loader__h2.arabic.active', {
-        opacity: 1,
-        pointerEvents: 'none'
-    })
+	tl.set(".loader__h2.arabic.active", {
+		opacity: 1,
+		pointerEvents: "none",
+	});
 
-    tl.to('.loader__h2.active .word', {
-        y: 0,
-        duration: 0.8,
-        stagger: { amount: 0.4 },
-        ease: 'Power1.easeInOut'
-    })
+	tl.to(".loader__h2.active .word", {
+		y: 0,
+		duration: 0.8,
+		stagger: { amount: 0.4 },
+		ease: "Power1.easeInOut",
+	});
 
-    tl.to(
-        '.loader__h2.active .word',
-        {
-            yPercent: -100,
-            duration: 0.8,
-            stagger: { amount: 0.4 },
-            ease: 'Power1.easeInOut'
-        },
-        '+=.2'
-    )
+	tl.to(
+		".loader__h2.active .word",
+		{
+			yPercent: -100,
+			duration: 0.8,
+			stagger: { amount: 0.4 },
+			ease: "Power1.easeInOut",
+		},
+		"+=.2",
+	);
 
-    tl.to(
-        '.loader',
-        {
-            yPercent: -100,
-            stagger: 0.4,
-            duration: 0.8,
-            ease: 'Power4.easeInOut',
-            delay: 0.2
-        },
-        '-=0.5'
-    )
+	tl.to(
+		".loader",
+		{
+			yPercent: -100,
+			stagger: 0.4,
+			duration: 0.8,
+			ease: "Power4.easeInOut",
+			delay: 0.2,
+		},
+		"-=0.5",
+	);
 
-    initPageAnimations(tl)
-}
+	initPageAnimations(tl);
+};
 
 export const initLoaderHome = () => {
-    const tl = gsap.timeline({})
-    initHomePage()
+	const tl = gsap.timeline({});
+	initHomePage();
 
-    //NOTE: starting the animation
+	//NOTE: starting the animation
 
-    tl.to('.loader__h2.active .char', {
-        y: 0,
-        stagger: { amount: 0.4 },
-        duration: 0.5
-    })
+	tl.to(".loader__h2.active .char", {
+		y: 0,
+		stagger: { amount: 0.4 },
+		duration: 0.5,
+	});
 
-    tl.to(
-        '.loader__h2.active .char',
-        {
-            yPercent: -100,
-            stagger: { amount: 0.4 },
-            duration: 0.5
-        },
-        '+=0.5'
-    )
+	tl.to(
+		".loader__h2.active .char",
+		{
+			yPercent: -100,
+			stagger: { amount: 0.4 },
+			duration: 0.5,
+		},
+		"+=0.5",
+	);
 
-    tl.to(
-        '.loader',
-        {
-            yPercent: -100,
-            stagger: 0.4,
-            duration: 0.8,
-            ease: 'Power4.easeInOut',
-            delay: 0.2
-        },
-        '<.5'
-    )
+	tl.to(
+		".loader",
+		{
+			yPercent: -100,
+			stagger: 0.4,
+			duration: 0.8,
+			ease: "Power4.easeInOut",
+			delay: 0.2,
+		},
+		"<.5",
+	);
 
-    initHomePageAnimations(tl)
-}
+	initHomePageAnimations(tl);
+};
 
 export const initPageLeave = () => {
-    const tl = gsap.timeline()
+	const tl = gsap.timeline();
 
-    tl.set('.loader', {
-        yPercent: 100
-    })
+	tl.set(".loader", {
+		yPercent: 100,
+	});
 
-    tl.to('.loader_second', {
-        yPercent: 0,
-        duration: 0.8,
-        ease: 'Power4.easeInOut'
-    })
+	tl.to(".loader_second", {
+		yPercent: 0,
+		duration: 0.8,
+		ease: "Power4.easeInOut",
+	});
 
-    tl.to(
-        '.loader_first',
-        {
-            yPercent: 0,
-            duration: 0.8,
-            ease: 'Power4.easeInOut',
-            delay: 0.2
-        },
-        '-=.7'
-    )
-}
+	tl.to(
+		".loader_first",
+		{
+			yPercent: 0,
+			duration: 0.8,
+			ease: "Power4.easeInOut",
+			delay: 0.2,
+		},
+		"-=.7",
+	);
+};
 export const initPageEnter = () => {
-    const tl = gsap.timeline()
-    new SplitType('.arabic', { types: 'lines,words' })
-    console.log(window.location.pathname)
+	const tl = gsap.timeline();
+	new SplitType(".arabic", { types: "lines,words" });
+	console.log(window.location.pathname);
 
-    tl.set('.loader__h2.init', {
-        opacity: 0,
-        pointerEvents: 'none'
-    })
+	tl.set(".loader__h2.init", {
+		opacity: 0,
+		pointerEvents: "none",
+	});
 
-    tl.set('.loader__h2.arabic.active', {
-        opacity: 1,
-        pointerEvents: 'none'
-    })
+	tl.set(".loader__h2.arabic.active", {
+		opacity: 1,
+		pointerEvents: "none",
+	});
 
-    tl.to(
-        '.loader__h2.active .word',
-        {
-            y: 0,
-            duration: 0.8,
-            stagger: { amount: 0.4 },
-            ease: 'Power1.easeInOut'
-        },
-        '+=.2'
-    )
+	tl.to(
+		".loader__h2.active .word",
+		{
+			y: 0,
+			duration: 0.8,
+			stagger: { amount: 0.4 },
+			ease: "Power1.easeInOut",
+		},
+		"+=.2",
+	);
 
-    tl.to(
-        '.loader__h2.active .word',
-        {
-            yPercent: -100,
-            duration: 0.8,
-            stagger: { amount: 0.4 },
-            ease: 'Power1.easeInOut'
-        },
-        '+=.2'
-    )
+	tl.to(
+		".loader__h2.active .word",
+		{
+			yPercent: -100,
+			duration: 0.8,
+			stagger: { amount: 0.4 },
+			ease: "Power1.easeInOut",
+		},
+		"+=.2",
+	);
 
-    tl.to(
-        '.loader',
-        {
-            yPercent: -100,
-            stagger: 0.4,
-            duration: 0.8,
-            ease: 'Power4.easeInOut',
-            delay: 0.2
-        },
-        '-=0.5'
-    )
+	tl.to(
+		".loader",
+		{
+			yPercent: -100,
+			stagger: 0.4,
+			duration: 0.8,
+			ease: "Power4.easeInOut",
+			delay: 0.2,
+		},
+		"-=0.5",
+	);
 
-    const location = window.location.pathname
+	const location = window.location.pathname;
 
-    if (location === '/index.html') {
-        initHomePage()
-        initHomePageAnimations(tl)
-    } else {
-        initPageAnimations(tl)
-        //
-        // if (window.location.pathname === './about.html') {
-        //     col_1();
-        // }
-    }
-}
+	if (location === "/index.html") {
+		initHomePage();
+		initHomePageAnimations(tl);
+	} else {
+		initPageAnimations(tl);
+		//
+		// if (window.location.pathname === './about.html') {
+		//     col_1();
+		// }
+	}
+};
 
 export const initPageAnimations = (tl: gsap.core.Timeline) => {
-    //NOTE: spliting headers
-    new SplitType('.once-in-title', { types: 'lines,words' })
+	//NOTE: splitting headers
+	new SplitType(".once-in-title", { types: "lines,words" });
 
-    //NOTE: setting values
-    gsap.set('.once-in-view img', {
-        scale: 1.8,
-        transformOrigin: 'center'
-    })
+	//NOTE: setting values
+	gsap.set(".once-in-view img", {
+		scale: 1.8,
+		transformOrigin: "center",
+	});
 
-    gsap.set('.once-in-title .line', {
-        overflow: 'hidden'
-    })
+	gsap.set(".once-in-title .line", {
+		overflow: "hidden",
+	});
 
-    gsap.set('.once-in-title .word', {
-        yPercent: 120
-    })
+	gsap.set(".once-in-title .word", {
+		yPercent: 120,
+	});
 
-    tl.to(
-        '.once-in-view img',
-        {
-            scale: 1.4,
-            duration: 0.8,
-            ease: 'Power4.easeInOut',
-            delay: 0.2
-        },
-        '-=.9'
-    )
+	tl.to(
+		".once-in-view img",
+		{
+			scale: 1.4,
+			duration: 0.8,
+			ease: "Power4.easeInOut",
+			delay: 0.2,
+		},
+		"-=.9",
+	);
 
-    tl.to(
-        '.once-in-title .word',
-        {
-            yPercent: 0,
-            stagger: { amount: 0.4 },
-            duration: 0.8,
-            ease: 'Power1.easeInOut'
-        },
-        '<.2'
-    )
-}
+	tl.to(
+		".once-in-title .word",
+		{
+			yPercent: 0,
+			stagger: { amount: 0.4 },
+			duration: 0.8,
+			ease: "Power1.easeInOut",
+		},
+		"<.2",
+	);
+};
 
 export function initNextWord(data: ITransitionData) {
-    // update Text Loading https://github.com/barbajs/barba/issues/507
-    const parser = new DOMParser()
-    const dom = parser.parseFromString(data.next.html, 'text/html')
-    const nextProjects = dom.querySelector('.loading-words')
-    document.querySelector('.loading-words')!.innerHTML = nextProjects!.innerHTML
+	// update Text Loading https://github.com/barbajs/barba/issues/507
+	const parser = new DOMParser();
+	const dom = parser.parseFromString(data.next.html, "text/html");
+	const nextProjects = dom.querySelector(".loading-words");
+	document.querySelector(".loading-words")!.innerHTML = nextProjects!.innerHTML;
 }
 
 export function delay(n: number) {
-    n = n || 2000
-    return new Promise<void>((done) => {
-        setTimeout(() => {
-            done()
-        }, n)
-    })
+	n = n || 2000;
+	return new Promise<void>((done) => {
+		setTimeout(() => {
+			done();
+		}, n);
+	});
 }
